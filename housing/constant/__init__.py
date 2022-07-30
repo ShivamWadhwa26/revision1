987bbaa052e9ff8c554c859539176478f2b3540e
ShivamWadhwa26 <shivamwadhwa.du.or.19@gmail.com>
2022-07-30 18:49:12 +0530

    data validation component WIP

--- housing/constant/__init__.py
+++ housing/constant/__init__.py
@@ -33,5 +33,9 @@ DATA_INGESTION_TEST_DIR_KEY = "ingested_test_dir"
 # Data Validation related variable
 
 # Data Validation related variables
-DATA_VALIDATION_CONFIG_KEY = "data_validation_config"
-DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name"
+DATA_VALIDATION_CONFIG_KEY = "data_validation_config" #key
+DATA_VALIDATION_SCHEMA_FILE_NAME_KEY = "schema_file_name" #schema.yaml
+DATA_VALIDATION_SCHEMA_DIR_KEY = "schema_dir" #config
+DATA_VALIDATION_ARTIFACT_DIR_NAME="data_validation" #
+DATA_VALIDATION_REPORT_FILE_NAME_KEY = "report_file_name" #report.json
+DATA_VALIDATION_REPORT_PAGE_FILE_NAME_KEY = "report_page_file_name" #report.html
